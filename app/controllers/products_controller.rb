ad6169e52c859790b91f7392c1c8aef47b2f5a21
Joshua Jacobs <josh@mandala-designs.com>
2010-03-14 17:54:55 -0400

    GREEN Products tests pass

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,27 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
+    
   end
 
-  def show
+  def show(product = nil)
+    @product ||= Product.find(params[:id])
   end
 
-  def new
+  def new(product = nil)
+    @product = product || Product.new
+    render :action => :new
   end
 
   def create
+    @product ||= Product.new(params[:product])
+    if @product.save
+      logger.info "CREATE PRODUCT: #{@product.id}"
+      flash[:success] = "Created title #{@product.title}"
+      redirect_to @product
+    else
+      self.new @product
+    end
   end
 
 end
