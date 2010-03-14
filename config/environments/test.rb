743a9e0288007fdb46216b8c56fc1862c8eff61d
James A. Rosen <james.a.rosen@gmail.com>
2010-03-13 21:40:53 -0500

    added shoulda, factory_girl, mocha, and redgreen for testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,3 +26,8 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
+
+config.gem 'shoulda',       :version => '~> 2.10.3',  :source => 'http://gems.rubyforge.org/'
+config.gem 'factory_girl',  :version => '~> 1.2.3',   :source => 'http://gems.rubyforge.org/'
+config.gem 'mocha',         :version => '~> 0.9.8',   :source => 'http://gems.rubyforge.org/'
+config.gem 'redgreen',      :version => '~> 1.2.2',   :source => 'http://gems.rubyforge.org/'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
+require 'shoulda'
+require 'factory_girl'
+require 'mocha'
+require 'redgreen'
 
 class ActiveSupport::TestCase
   # Transactional fixtures accelerate your tests by wrapping each test method
