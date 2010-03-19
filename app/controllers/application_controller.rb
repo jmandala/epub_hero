d46b4c613fa9f89ac7620e4389c55583ee631b9f
Joshua Jacobs <josh@mandala-designs.com>
2010-03-19 00:03:47 -0400

    added custom handling for error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,19 @@ class ApplicationController < ActionController::Base
   
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  
+  
+  ActionView::Base.field_error_proc = Proc.new do |html_tag, instance| 
+    error_message = instance.error_message
+    if instance.error_message.kind_of?(Array)  
+      error_message = instance.error_message.join(',')
+    end
+    
+    if html_tag =~ /radio/i
+      html_tag
+    else
+      %(<div class="fieldWithErrors">#{html_tag}<span class="validation-error">&nbsp;#{error_message}</span></div>)
+    end
+  end 
+  
 end
